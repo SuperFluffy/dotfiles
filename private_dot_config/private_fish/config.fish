fbf3c09b929470f149e9d21d5851f9f3738f59be
Richard Janis Goldschmidt <github@aberrat.io>
2021-07-04 16:16:08 +0200

    set ssh auth sock on macos

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -4,6 +4,10 @@ set -gx GPG_TTY (tty)
 set -gx FZF_DEFAULT_COMMAND 'rg --files'
 set -gx VOLTA_HOME "$HOME/.volta"
 
+if string match -iq -- darwin (uname) 
+	set -gx SSH_AUTH_SOCK "$HOME/.gnupg/S.gpg-agent.ssh"
+end
+
 if status is-login
 	if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
 		exec startx -- -keeptty
