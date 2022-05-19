ae6f635f15c9e347dacd0b19b3e5ee2c2c85d701
Janis Goldschmidt <janis@promoted.ai>
2022-05-19 14:55:22 +0200

    Also include cmp config

---
+++ private_dot_config/nvim/lua/config/cmp.lua
@@ -0,0 +1,27 @@
+local cmp = require('cmp')
+local opts = {
+  snippet = {
+    expand = function(args)
+        vim.fn["vsnip#anonymous"](args.body)
+    end,
+  },
+  mapping = {
+    ['<C-p>'] = cmp.mapping.select_prev_item(),
+    ['<C-n>'] = cmp.mapping.select_next_item(),
+    -- Add tab support
+    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.close(),
+    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+  },
+
+  -- Installed sources
+  sources = {
+    { name = 'nvim_lsp' },
+    { name = 'vsnip' },
+    { name = 'path' },
+    { name = 'buffer' },
+  },
+}
+cmp.setup(opts)
