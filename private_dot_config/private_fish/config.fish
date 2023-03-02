8f3408f5a60738d71b4aef733a8d04bdc41e1999
Janis Goldschmidt <github@aberrat.io>
2023-03-02 16:26:00 +0100

    long overdue update

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -1,13 +1,16 @@
 starship init fish | source
 
-set -gx GPG_TTY (tty)
+set -gx EDITOR helix
 set -gx FZF_DEFAULT_COMMAND 'rg --files'
 set -gx VOLTA_HOME "$HOME/.volta"
 
+set -e SSH_AGENT_PID
+set -gx SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
+
 if string match -iq -- darwin (uname) 
-	set -gx SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
 	gpgconf --launch gpg-agent
 end
+set -gx GPG_TTY (tty)
 
 if status is-login
 	if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
